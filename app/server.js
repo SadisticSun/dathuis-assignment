e79bee6b3aa447d5da401610818ce13e92e2165a
Tristan Jacobs <tristan@iamt.nl>
2018-08-07 09:33:18 +0200

    WIP: graphql search query

--- app/server.js
+++ app/server.js
@@ -1,7 +1,48 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 8081
 const dummyData = require('./data/clients.json')
+const express_graphql = require('express-graphql')
+const {
+    buildSchema
+} = require('graphql')
+
+const schema = buildSchema(`
+    type Query {
+        persons(first_name: String): [Person]
+    },
+    
+    type Person {
+        first_name: String,
+        last_name: String,
+        email: String,
+        gender: String,
+        photo: String,
+        origin: String,
+      }
+`)
+
+const getPersons = function (args) {
+    console.log(args)
+    // const firstName = args.first_name,
+    //     lastName = args.last_name,
+    //     origin = args.origin;
+
+    return dummyData.filter(person => person.first_name.indexOf(args.first_name) !== -1).slice(0, 10)
+}
+
+const rootResolver = {
+    message: () => 'Hello GraphQL',
+    persons: getPersons
+}
+
+
+
+app.use('/graphql', express_graphql({
+    schema: schema,
+    rootValue: rootResolver,
+    graphiql: true
+}))
 
 
 app.get('/api/data', (req, res) => {
@@ -9,5 +50,5 @@ app.get('/api/data', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server started on ${port}`)
+    console.log(`Server started on http://localhost:${port}`)
 })
